1a38c9dd130bbdad8314064094606168cb15b298
Pancakeddd <chaneofcake@gmail.com>
2019-12-24 13:38:00 -0500

    Fixing parser

--- tundra/main.lua
+++ tundra/main.lua
@@ -3,6 +3,12 @@ inspect = require("debugkit.inspect").inspect
 local matchString
 matchString = require("tundra.parser").matchString
 return print(inspect(matchString([[
-  z apple.
+  atom.
+  [a., b.]
+  a = b.
+  a
+
+  a b. c.
+  a (b. c.)
 
 ]])))
--- tundra/parser.lua
+++ tundra/parser.lua
@@ -4,12 +4,18 @@ do
   P, S, R, C, V, Ct, B = _obj_0.P, _obj_0.S, _obj_0.R, _obj_0.C, _obj_0.V, _obj_0.Ct, _obj_0.B
 end
 local w = S(" \t\r\n") ^ 0
-local wstop = P("\n") ^ 0
+local space = S(" \t") ^ 0
+local wstop = P("\n")
 local digit = R("09")
 local number = C(digit ^ 1)
 local letter = R("az") + R("AZ") + P("_")
 local word = C(letter ^ 1)
 local dot_word = word * P(".")
+local comment = P("--") * (1 - S("\r\n")) ^ 0 * wstop
+local stop
+stop = function(e)
+  return e - wstop
+end
 local Node
 Node = function(name)
   return function(...)
@@ -25,15 +31,17 @@ local tundra_parser = P({
   "tundra",
   tundra = w * V("body"),
   body = (V("statement") + V("expression")) ^ 0 / Node("body"),
-  statement = V("assignment") + V("list"),
+  statement = V("container") + V("assignment") + V("list"),
   atom = w * dot_word / Node("atom"),
   identifier = w * word / Node("ref"),
   number = w * number / Node("number"),
-  real_atom = V("atom") + V("identifier") + V("number"),
+  named = V("atom") + V("identifier"),
+  real_atom = V("named") + V("number"),
   group = w * P("(") * w * V("expression") * w * P(")") * w / Node("group"),
   expression = V("call") + V("group") + V("real_atom"),
-  call = V("identifier") * w * ((V("atom") + V("identifier")) + V("expression")) ^ 1 * wstop / Node("call"),
+  call = V("named") * space * ((V("named") + V("expression")) - wstop) ^ 1 / Node("call"),
   assignment = V("identifier") * w * P("=") * w * V("expression") / Node("assignment"),
+  container = V("atom") * w * P(".=") * w * (V("list") + V("atom")) / Node("container"),
   list = w * P("[") * w * ((V("real_atom")) ^ 1 * (w * P(",") * w * V("real_atom")) ^ 0) * w * P("]") * w / Node("list")
 })
 local matchString
