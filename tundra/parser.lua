528492e55a8b6ef10fac7770493ea9812408d66a
Pancakeddd <chaneofcake@gmail.com>
2019-12-24 15:37:52 -0500

    Added anything wildcard

--- tundra/parser.lua
+++ tundra/parser.lua
@@ -42,7 +42,8 @@ local tundra_parser = P({
   call = V("named") * space * ((V("named") + V("expression")) - wstop) ^ 1 / Node("call"),
   assignment = V("identifier") * w * P("=") * w * V("expression") / Node("assignment"),
   wildcard = (number ^ 0 * P("*")) / Node("wildcard"),
-  container = V("atom") * w * P(".=") * w * (V("wildcard") + V("list") + V("atom")) / Node("container"),
+  wildcard_all = P("**") / Node("all_wildcard"),
+  container = V("atom") * w * P(".=") * w * (V("wildcard_all") + V("wildcard") + V("list") + V("atom")) / Node("container"),
   list = w * P("[") * w * ((V("real_atom")) ^ 1 * (w * P(",") * w * V("real_atom")) ^ 0) * w * P("]") * w / Node("list")
 })
 local matchString
